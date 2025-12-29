9e440c2a9d89d4aaf31580f193290d6a14b4e1b6
Arnab <arnabchoudhury2005@gmail.com>
2025-12-30 00:05:23 +0530

    Learing and building

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import "./Homepage.css"
 import Profile from "../assets/Profile.jpeg";
 import Hero from "../Components/Hero";
+import { useNavigate } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
@@ -9,6 +10,9 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { IoDocumentTextOutline } from "react-icons/io5";
 
 export function HomePage() {
+
+const navigate = useNavigate();
+
     return (
         <>
             <div className="Homepage">
@@ -22,7 +26,7 @@ export function HomePage() {
                     <h4>I’m a third-year BCA (Hons.) student at Parul University with a strong interest in software development. I enjoy turning ideas into real-world applications and have a growing passion for working with data.</h4>
                     <div className="Button">
                         <button className="Resumebutton"><IoDocumentTextOutline />View Resume</button>
-                        <button className="Myworkbutton">My Work <FaArrowRightLong /></button>
+                        <button onClick={() => navigate("/Project")} className="Myworkbutton">My Work <FaArrowRightLong /></button>
                     </div>
                     <div className="Homesocialmedia">
                         <a href="https://www.linkedin.com/in/arnab-choudhury12/" target="_blank"><FaLinkedin /></a>
