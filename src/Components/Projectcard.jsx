760640e197b9dfb58a6ad4ee4069078b9d6ad78e
Arnab <arnabchoudhury2005@gmail.com>
2025-12-30 19:40:03 +0530

    learning and building

---
+++ src/Components/Projectcard.jsx
@@ -0,0 +1,18 @@
+import "./Projectcard.css"
+export function Projectcard({Title, Discription, Tech, Livelink, Github}) {
+    return(
+        <>
+            <div className="Project-card">
+                <h3>{Title}</h3>
+                <p>{Discription}</p>
+
+                <div className="tech">{Tech}</div>
+
+                <div className="buttons">
+                    <a href={Livelink} className="Livelink" target="_blank">Live</a>
+                    <a href={Github} className="Githublink" target="_blank">GitHub</a>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -1,4 +1,5 @@
 import "./Projectpage.css"
+import { Projectcard } from "../Components/Projectcard";
 import { FaHammer } from "react-icons/fa6";
 import { LuConstruction } from "react-icons/lu";
 export function ProjectPage(){
@@ -9,7 +10,20 @@ export function ProjectPage(){
                 <h2><LuConstruction />Projects... Stay tuned!<FaHammer /></h2>
                 <h4>A learning journey through projects and real-world implementations, each one helping me learn something new.</h4>
             </div>
-            <div className="Projectcard">
+            <div className="Project">
+                <Projectcard 
+                    Title="Chatapp"
+                    Discription="A real-time chat application that allows users to communicate instantly, focusing on performance, usability, and a responsive design."
+                    Tech="React.js Express.js Node.js Tailwind Css Socket.io"
+                    Livelink="https://chat-app-wine-seven-52.vercel.app/"
+                    Github="https://github.com/itsmearnab12/Chatapp"
+                />
+                <Projectcard 
+                    Title="News Website"
+                    Discription="A dynamic news platform built to fetch and display real-time news with a modern and user-friendly interface."
+                    Tech="HTML Express.js Node.JS CSS News.io"
+                    Github="https://github.com/itsmearnab12/FullStackProjects/tree/main/Project%201"
+                />
             </div> 
         </>
     )
