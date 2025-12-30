a3756e7d16bce2669effc723beb09474323a05e9
Arnab <arnabchoudhury2005@gmail.com>
2025-12-30 17:00:26 +0530

    learning and building

--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -1,7 +1,16 @@
+import "./Projectpage.css"
+import { FaHammer } from "react-icons/fa6";
+import { LuConstruction } from "react-icons/lu";
 export function ProjectPage(){
     return(
         <>
-            Project Page 
+            <div className="Projectpage">
+                <h5>-My Projects</h5>
+                <h2><LuConstruction />Projects... Stay tuned!<FaHammer /></h2>
+                <h4>A learning journey through projects and real-world implementations, each one helping me learn something new.</h4>
+            </div>
+            <div className="Projectcard">
+            </div> 
         </>
     )
 }
